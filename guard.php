ff7acdedde5a15cbb8d4d2942b30167d325bdb1e
Abdul Muttaqin <31664438+fdciabdul@users.noreply.github.com>
2019-02-04 23:46:33 +0700

    Add files via upload

---
+++ guard.php
@@ -0,0 +1,58 @@
+<?php
+// Warna Terminal
+$biru = "\e[34m";
+$kuning = "\e[33m";
+$cyan = "\e[96m";
+$magenta = "\e[35m";
+$hijau = "\e[92m";
+$merah = "\e[91m";
+require_once('fb.php');
+echo "$cyan + //////////////////////////////+\n";
+echo "$cyan ___           _     _
+|_ _|_ __  ___(_) __| | ___
+ | || '_ \/ __| |/ _` |/ _ \
+ | || | | \__ \ | (_| |  __/
+|___|_| |_|___/_|\__,_|\___|\n";
+echo "_   _                 _
+| | | | ___  __ _ _ __| |_ ____
+| |_| |/ _ \/ _` | '__| __|_  /
+|  _  |  __/ (_| | |  | |_ / /
+|_| |_|\___|\__,_|_|   \__/___|\n\n";
+echo "+ //////////////////////////////+\n";
+echo "$Input Your Token Facebok: ";
+$token= trim(fgets(STDIN));
+$md5 = md5(time());
+$hash = substr($md5, 0, 8)."-".substr($md5, 8, 4)."-".substr($md5, 12, 4)."-".substr($md5, 16, 4)."-".substr($md5, 20, 12);
+function curl($url, $post=null) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    if($post != null) {
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+    }
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    $exec = curl_exec($ch);
+    curl_close($ch);
+    return $exec;
+}
+$me = json_decode(curl("https://graph.facebook.com/me?fields=id&access_token=".$token));
+if($me && $me->id) {
+    $var = "{\"0\":{\"is_shielded\":true,\"session_id\":\"$hash\",\"actor_id\":\"$me->id\",\"client_mutation_id\":\"$hash\"}}";
+    $hajar = json_decode(curl("https://graph.facebook.com/graphql", array(
+        "variables" => $var,
+        "doc_id" => "1477043292367183",
+        "query_name" => "IsShieldedSetMutation",
+        "strip_defaults" => "true",
+        "strip_nulls" => "true",
+        "locale" => "en_US",
+        "client_country_code" => "US",
+        "fb_api_req_friendly_name" => "IsShieldedSetMutation",
+        "fb_api_caller_class" => "IsShieldedSetMutation",
+        "access_token" => $token
+    )));
+    if($hajar->data->is_shielded_set->is_shielded) echo "$merah Success :) Check Your Profile Now";
+    else "Failed";
+}
+?>
